d4e4b7f959c8a227bb66a549725f459dd78a6fd9
Parth Agarwal <69203808+parth-dev1512@users.noreply.github.com>
2026-02-26 16:36:42 +0530

    Adjust login link margin in navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ const Navbar = {
             <a href="/#/search">Find Course</a>
             <a href="/#/register" class="btn btn-primary" style="padding: 0.5rem 1rem; font-size: 0.9rem;">Start Teaching</a>
             <div id="auth-links">
-              <a href="/#/login" style="margin-top: 4px; display: inline-block;">
+              <a href="/#/login" style="margin-top: 40px; display: inline-block;">
                 Log In
               </a>
             </div>
